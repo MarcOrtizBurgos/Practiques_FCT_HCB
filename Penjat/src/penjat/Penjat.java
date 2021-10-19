ec4b67ede37d332fa56cc6d7c4b32e40f5015c38
ortiz.burgos.marc <ortiz.burgos.marc@alumnat.copernic.cat>
2021-10-19 19:03:29 +0200

    mob - penjat amb petits errors acabant de finalitzar

--- Penjat/src/penjat/Penjat.java
+++ Penjat/src/penjat/Penjat.java
@@ -1,21 +1,95 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package penjat;
+
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
- * @author User
+ * @author Marc Ortiz Burgos
  */
 public class Penjat {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        
+        int acerts = 0, intents = 7;
+        String[] paraules = new String[]{"lapiz", "goma", "libreta", "maestro", "examen", "matematicas"};
+        
+        benvinguda();
+        joc(acerts, intents,paraules);
+        adeu();
+    }
+    
+    public static void benvinguda (){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Benvingut al penjat!");
+        System.out.println("Pulsa ENTER per començar");
+        in.nextLine();
+    }
+    
+    public static String paraulaRandom (String[] paraules){
+        Random rand = new Random();
+        int val = rand.nextInt(6);
+        return paraules[val];
+    }
+    
+    public static void joc (int acerts, int intents, String[] paraules){
+        Scanner in = new Scanner(System.in);
+        
+        String paraula = paraulaRandom(paraules);
+        
+        String[] ArraySep = paraula.split("");
+        String[] ArrayCon = new String[ArraySep.length];
+        for (int i = 0; i < ArraySep.length; i++){
+            ArrayCon[i] = "_";
+        }
+        
+        while(intents != 0 || acerts == ArraySep.length){
+            System.out.println("Acerts: "+acerts+" || Intents: "+intents);
+            for (int i = 0; i < ArraySep.length; i++){
+                if(ArrayCon[i] != ArraySep[i]){
+                    System.out.print(ArrayCon[i]);
+                }
+                else{
+                    System.out.print(ArraySep[i]);
+                }
+                System.out.print(" ");
+            }
+            
+            System.out.println("\n");
+            System.out.println("Escriu una lletra: ");
+            String lletra = in.nextLine();
+            
+            for (int x = 0; x < ArraySep.length; x++){
+                if(ArraySep[x].contains(lletra)){
+                    ArrayCon[x] = lletra;
+                }
+            }
+            
+            if(ArrayCon.toString().contains(lletra)){
+                acerts++;
+            }
+            else{
+                intents--;
+            }
+            
+        }
+        
+        if(intents == 0){
+            System.out.println("Has perdut :( , la teva paraula era "+paraula);
+        }
+        else if(acerts == ArraySep.length){
+            System.out.println("Has guanyat! :)");
+        }
+        
+    }
+    
+    
+    public static void adeu (){
+        Scanner in = new Scanner(System.in);
+        System.out.println(); System.out.println();
+        System.out.print("Pulsa ENTER per terminar... ");
+        in.nextLine();
     }
     
 }
