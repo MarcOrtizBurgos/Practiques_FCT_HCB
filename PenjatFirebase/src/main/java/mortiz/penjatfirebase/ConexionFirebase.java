e49e3b1faaae203236392aec52b1ad7ce54b7263
ortiz.burgos.marc <ortiz.burgos.marc@alumnat.copernic.cat>
2021-10-22 18:09:33 +0200

    mob - posar dependencies i conectar firebase

--- PenjatFirebase/src/main/java/mortiz/penjatfirebase/ConexionFirebase.java
+++ PenjatFirebase/src/main/java/mortiz/penjatfirebase/ConexionFirebase.java
@@ -1,9 +1,19 @@
 package mortiz.penjatfirebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import java.io.FileInputStream;
+import java.io.IOException;
+
 /**
  *
  * @author Marc Ortiz Burgos
  */
 public class ConexionFirebase {
-    
+    public static void conectar() throws IOException{
+        FileInputStream serviceAccount = new FileInputStream("path/to/fir-penjat-firebase.json");
+        FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
+        FirebaseApp.initializeApp(options);
+    }
 }
