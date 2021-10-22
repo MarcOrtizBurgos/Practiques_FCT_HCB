7f20e3e7441c767c90fb6f4249400bc11c931f5b
ortiz.burgos.marc <ortiz.burgos.marc@alumnat.copernic.cat>
2021-10-22 18:58:22 +0200

    mob - conexion echa

--- PenjatFirebase/src/main/java/mortiz/penjatfirebase/ConexionFirebase.java
+++ PenjatFirebase/src/main/java/mortiz/penjatfirebase/ConexionFirebase.java
@@ -1,8 +1,10 @@
 package mortiz.penjatfirebase;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -11,9 +13,22 @@ import java.io.IOException;
  * @author Marc Ortiz Burgos
  */
 public class ConexionFirebase {
+    
+    static Firestore bd;
+    
     public static void conectar() throws IOException{
-        FileInputStream serviceAccount = new FileInputStream("path/to/fir-penjat-firebase.json");
+        FileInputStream serviceAccount = new FileInputStream("fir-penjat-firebase.json");
         FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount)).build();
         FirebaseApp.initializeApp(options);
+        bd = FirestoreClient.getFirestore();
+        System.out.println("S'ha conectat amb exit");
+    }
+    
+    public static void main(String[] args){
+        try{
+            conectar();
+        }catch(Exception e){
+            System.out.println("Va haver-hi un error en la connexió");
+        }
     }
 }
