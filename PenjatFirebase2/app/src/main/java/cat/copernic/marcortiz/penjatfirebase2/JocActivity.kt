5886f363010691907c68a16eebdff571ca1ae81a
ortiz.burgos.marc <ortiz.burgos.marc@alumnat.copernic.cat>
2021-11-17 17:47:45 +0100

    mob - treure comentaris innecessaris

--- PenjatFirebase2/app/src/main/java/cat/copernic/marcortiz/penjatfirebase2/JocActivity.kt
+++ PenjatFirebase2/app/src/main/java/cat/copernic/marcortiz/penjatfirebase2/JocActivity.kt
@@ -37,14 +37,12 @@ class JocActivity : AppCompatActivity() {
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-                    //Log.d(TAG, "${document.id} => ${document.data}")
                     if (document.id.toString().equals(partida.toString())){
                         textpartida.setText(document.id)
                         intents.setText(document.get("intents").toString())
                         acerts.setText(document.get("acerts").toString())
                         iniciar(document)
                     }
-                    //Log.d("MainActivity", "${document.id} => ${document.data}")
                 }
             }
             .addOnFailureListener { exception ->
@@ -57,10 +55,7 @@ class JocActivity : AppCompatActivity() {
                 .addOnSuccessListener { result ->
                     for (document in result) {
                         if (document.id.equals(partida)) {
-                            //Log.d(TAG, "${document.id} => ${document.data}")
-                            //joc(partida, document)
                             newjoc(partida, document)
-                            //Log.d("MainActivity", "${document.id} => ${document.data}")
                         }
                     }
                 }
@@ -95,16 +90,11 @@ class JocActivity : AppCompatActivity() {
 
     public fun newjoc(partida: String, document: DocumentSnapshot) {
         //Iniciar si es te que iniciar algun valor
-
         //Primer fer comprovacio de la lletra
         //Depenent del tipus de lletra if si es valida else no
-
         //Si es valida s'actualitzan valors acerts +1
-
         //Si es negativa s'actualitzen valors intents -1
-
         //Ultim actualitzar els valors per comprovació
-
         //Comprovació si has guanyat , perdut o pots seguir
 
         var lletreambespai = ""
@@ -200,7 +190,6 @@ class JocActivity : AppCompatActivity() {
                 )
                 intents.setText(document.get("intents").toString())
                 acerts.setText(document.get("acerts").toString())
-                //paraulatext.setText(lletreambespai.toString())
             }
 
         }
@@ -246,123 +235,6 @@ class JocActivity : AppCompatActivity() {
             2000 // value in milliseconds
         )
     }
-
-    /*
-    @Throws(InterruptedException::class, ExecutionException::class)
-    fun joc(partida: String, document: DocumentSnapshot) {
-        var nomp: String?
-        nomp = partida
-        //Escull paraula random de l'array.
-        val paraula = document.get("paraula").toString() as String
-        //Array de les lletras de la paraula.
-        val ArraySep = paraula.split("").toTypedArray()
-        //Array buit amb la longitud del primer array amb la paraula.
-        val ArrayCon = arrayOfNulls<String>(ArraySep.size)
-        //String adivina amb les lletres trobades
-        val adivina = document.get("adivinar").toString() as String
-        //For per posar amb _ l'array buida o les lletres que ja estan descobertes
-        for (i in ArraySep.indices) {
-            if (adivina.toString().contains(ArraySep[i].toString())) {
-                ArrayCon[i] = ArraySep[i].toString()
-            } else {
-                ArrayCon[i] = "_"
-            }
-        }
-        var rep = false
-        //Si perds o guanyes s'acaba l'ha partida sino segueix el joc
-        if (document.get("intents").hashCode() as Int == 0) {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Error")
-            builder.setMessage("Has perdut :( , la teva paraula era $paraula")
-            builder.setPositiveButton("Aceptar", null)
-            val dialog: AlertDialog = builder.create()
-            dialog.show()
-            finalitzat(nomp, "perdut")
-        } else if (document.get("acerts").hashCode() as Int == ArraySep.size) {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Error")
-            builder.setMessage("Has guanyat! :) , la paraula es $paraula")
-            builder.setPositiveButton("Aceptar", null)
-            val dialog: AlertDialog = builder.create()
-            dialog.show()
-            finalitzat(nomp, "guanyat")
-        } else {
-            //acerts i intents
-
-            //Imprimeix l'array amb les lletres ocultes.
-            var lletreambespai = ""
-            for (i in ArraySep.indices) {
-                if (ArrayCon[i] !== ArraySep[i]) {
-                    lletreambespai += ArrayCon[i].toString()+" "
-                } else {
-                    lletreambespai += ArraySep[i].toString()+" "
-                }
-            }
-            paraulatext.setText(lletreambespai)
-
-            //Pregunta lletra.
-            val lletra: String = editLletra.text.toString()
-
-            //Comproba que ya esta posada o no a l'array
-            for (x in ArrayCon.indices) {
-                if (ArrayCon[x]!!.contains(lletra.toString())) {
-                    rep = true
-                }
-            }
-            if (rep) {
-                val builder = AlertDialog.Builder(this)
-                builder.setTitle("Error")
-                builder.setMessage("Aquest caracter ja l'has posat")
-                builder.setPositiveButton("Aceptar", null)
-                val dialog: AlertDialog = builder.create()
-                dialog.show()
-            } else {
-                //Si es una lletra segueix el joc sino es torna a preguntar.
-                if (!Character.isLetter(lletra[0])) {
-                    val builder = AlertDialog.Builder(this)
-                    builder.setTitle("Error")
-                    builder.setMessage(
-                        "Aquest caracter no es correcte.\n"
-                                + "Torna a probar."
-                    )
-                    builder.setPositiveButton("Aceptar", null)
-                    val dialog: AlertDialog = builder.create()
-                    dialog.show()
-                } else {
-                    //Recorreix la array per comprobar si hi ha lletras que coincideixen.
-                    var check = false
-                    for (x in ArraySep.indices) {
-                        if (ArraySep[x].toString().contains(lletra.toString())) {
-                            ArrayCon[x] = lletra.toString()
-                            check = true
-                            updateData(
-                                nomp.toString(),
-                                document.get("intents").hashCode() as Int,
-                                document.get("acerts").hashCode() as Int + 1,
-                                document.get("adivinar").toString() + lletra.toString()
-                            )
-                            intents.setText(document.data?.get("intents")?.toString())
-                            acerts.setText(document.data?.get("acerts")?.toString())
-                            //paraulatext.setText(lletreambespai.toString())
-                        }
-                    }
-                    //Si no coincideix ninguna lletra et treu un intent.
-                    if (!check) {
-                        imagenes(document)
-                        updateData(
-                            nomp.toString(),
-                            document.get("intents").hashCode() as Int - 1,
-                            document.get("acerts").hashCode(),
-                            document.get("adivinar").toString() + lletra.toString()
-                        )
-                        intents.setText(document.data?.get("intents")?.toString())
-                        acerts.setText(document.data?.get("acerts")?.toString())
-                        //paraulatext.setText(lletreambespai.toString())
-                    }
-                }
-            }
-        }
-    }*/
 
     //Finalitza la partida a la base de dades
     fun finalitzat(nomp: String?, estat: String) {
